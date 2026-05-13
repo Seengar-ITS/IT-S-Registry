11b10d6bd94365a3113cbe0815218cadc84b01d1
Seengar <seengar237@gmail.com>
2026-05-14 04:33:57 +0500

    fix: correct hyphenated import identifiers causing build failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
-import My-packages from './pages/My-packages'
+import MyPackages from './pages/My-packages'
 import Publish from './pages/Publish'
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/my-packages" element={<My-packages />} />
+          <Route path="/my-packages" element={<MyPackages />} />
           <Route path="/publish" element={<Publish />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
